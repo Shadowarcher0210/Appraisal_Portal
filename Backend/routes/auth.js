f6def6543df4b6f3e8c22d8ca5a0e1313cd82f13
Shadowarcher0210 <150799804+Shadowarcher0210@users.noreply.github.com>
2024-09-30 17:07:25 +0530

    first

---
+++ Backend/routes/auth.js
@@ -0,0 +1,34 @@
+// routes/auth.js
+const express = require('express');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+
+// Register route
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ username, password: hashedPassword });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Login route
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+  if (!user) return res.status(404).json({ message: 'User not found' });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+
+  res.status(200).json({ message: 'Login successful', user });
+});
+
+module.exports = router;
